ef3d2ff85ee6238038e1ef746eb7fe1de1927ab8
filipedpsilva <filipedpsilva@protonmail.com>
2019-05-11 22:57:08 +0000

    Added built website

---
+++ megaloja/build/precache-manifest.12285f86efabf283d00c433859ef91cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6b9cb396bec58fd64d58239380a710",
+    "url": "/megaloja/index.html"
+  },
+  {
+    "revision": "3beff4f885b0fb375094",
+    "url": "/megaloja/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "54b546de418e8c779574",
+    "url": "/megaloja/static/css/main.2f66e001.chunk.css"
+  },
+  {
+    "revision": "3beff4f885b0fb375094",
+    "url": "/megaloja/static/js/2.dbe9cb5e.chunk.js"
+  },
+  {
+    "revision": "54b546de418e8c779574",
+    "url": "/megaloja/static/js/main.54f32aec.chunk.js"
+  },
+  {
+    "revision": "823360570a119cede5f2",
+    "url": "/megaloja/static/js/runtime~main.d5830049.js"
+  },
+  {
+    "revision": "4c956e08041a3bd2bfe894ae8b6d9c30",
+    "url": "/megaloja/static/media/about_us.4c956e08.jpg"
+  },
+  {
+    "revision": "7e538212bdc039c3c8c5d1ea3428c833",
+    "url": "/megaloja/static/media/construction_flyer.7e538212.pdf"
+  },
+  {
+    "revision": "e63ecd110a359c086cdb47dfa1860a89",
+    "url": "/megaloja/static/media/furniture_flyer.e63ecd11.pdf"
+  },
+  {
+    "revision": "6067068d52658b0692443da16cc2054a",
+    "url": "/megaloja/static/media/google_maps.6067068d.png"
+  },
+  {
+    "revision": "c38fec815e3de83e5406e4a378330a51",
+    "url": "/megaloja/static/media/site_logo.c38fec81.png"
+  }
+]);
