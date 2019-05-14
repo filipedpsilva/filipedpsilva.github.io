0fef4fe32906b9c04fefccdeab38302960c63ced
filipedpsilva <filipedpsilva@protonmail.com>
2019-05-14 21:58:33 +0000

    New deploy.

--- megaloja/service-worker.js
+++ megaloja/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/megaloja/precache-manifest.2bacc00f1c7cb177f8ad21d2a08d1ca4.js"
+  "/precache-manifest.8917071455956a001e5b7582bc60df19.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/megaloja/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
